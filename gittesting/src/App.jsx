2c38c2b922f30393ef1feb00a15618ebc0379073
pramodsriramula <pramodsriramulu123@gmail.com>
2026-09-14 15:00:39 +0530

    TestEdit0300pm

--- gittesting/src/App.jsx
+++ gittesting/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React - this is an edit to an edit
+          Learn React & vite - Test
         </a>
       </header>
     </div>
